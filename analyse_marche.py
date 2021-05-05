b6fef19f9dfb473c3f8f2e7fd03a6e327d14521c
chekna75 <chekna75@github.com>
2021-05-05 14:45:41 +0200

    emmit

--- analyse_marche.py
+++ analyse_marche.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/'    #crée une variable pour l'url
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'    #crée une variable pour l'url
 reponse = requests.get(url)
 
 if reponse.ok:
     soup = BeautifulSoup(reponse.text, 'html')
-    title = soup.find('title')
+    title = soup.find('title') #chercher le titre
+    trs = soup.findAll('tr') # chercher tout les tr
+    tds = soup.findAll('td')
+    h1 = soup.find('h1')
 
-print (title)
+print (url) # afficher l'url
+print (title.text)
+print (h1.text)
+[print(str(tr) + '\n\n') for tr in trs] # boucle qui sert a afficher tout les tr
