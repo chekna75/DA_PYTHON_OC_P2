b03a873ed93a12743305a9550a068caf0858902c
chekna75 <chekna75@github.com>
2021-05-06 08:56:35 +0200

    soup.select_one

--- analyse_marche.py
+++ analyse_marche.py
@@ -5,7 +5,9 @@ url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 reponse = requests.get(url)
 
 if reponse.ok:
+    #tbody > tr:nth-child(1) > td
     soup = BeautifulSoup(reponse.text, 'html')
+    upc = soup.select_one("table tr:nth-child(1) > td").text 
     title = soup.find('title') #chercher le titre
     trs = soup.findAll('tr') # chercher tout les tr
     tds = soup.findAll('td')
@@ -14,4 +16,6 @@ if reponse.ok:
 print (url) # afficher l'url
 print (title.text)
 print (h1.text)
-[print(str(tr) + '\n\n') for tr in trs] # boucle qui sert a afficher tout les tr
+[print(str(tr.text) + '\n') for tr in trs] # boucle qui sert a afficher tout les tr
+
+#dict() pour renvoyer les valeurs
