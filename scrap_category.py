287e32d22923678a017e73dd1312a33fbe1343a1
chekna75 <chekna75@github.com>
2021-05-23 16:08:03 +0200

    on scrap tout les livres d'une categorie

--- scrap_category.py
+++ scrap_category.py
@@ -3,8 +3,24 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrap_category(url):
-    reponse = requests.get(url)
-    if reponse.ok:
-        soup = BeautifulSoup(reponse.content, "html.parser")
-        return [item.get('href').replace('../../..','http://books.toscrape.com/catalogue') for item in soup.select('section h3 a')]
-        #replace sert a remplacer le texte (avec le premier parametre a changer, et le 2eme parametre avec quoi on veut modiufier)
+    books_urls = [] #Création d'un dictionnaire
+    for u in generate_category_url(url): # Boucle qui recupère l'url et génère les url
+        reponse = requests.get(u) 
+        if reponse.ok:
+            soup = BeautifulSoup(reponse.content, "html.parser")
+            books_urls += [item.get('href').replace('../../..','http://books.toscrape.com/catalogue') for item in soup.select('section h3 a')] 
+            #On ajouter les url a books urls
+        else:
+            break
+    return books_urls
+
+        
+        
+
+def generate_category_url(url):
+    nb = 1
+    yield url
+    while True:
+        nb += 1
+        yield url.replace('index',f'page-{nb}')
+print(scrap_category('http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html'))
--- scrap_list_category.py
+++ scrap_list_category.py
@@ -7,7 +7,7 @@ def scrap_list_category(url):
     if reponse.ok:
         soup = BeautifulSoup(reponse.content, "html.parser")
         ins = 'http://books.toscrape.com/'
-        return [item.get('href') for item in soup.select('div.side_categories > ul > li > ul > li > a')]
+        return [ins + item.get('href') for item in soup.select('div.side_categories > ul > li > ul > li > a')]
         #replace sert a remplacer le texte (avec le premier parametre a changer, et le 2eme parametre avec quoi on veut modiufier)
 if __name__ == '__main__' :
     print(scrap_list_category('http://books.toscrape.com/')) 
