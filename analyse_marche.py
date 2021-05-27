29c402c3284e0a414986581bfb2e2b71db1d08a9
chekna75 <chekna75@github.com>
2021-05-27 15:18:54 +0200

    mise au point du csv

--- analyse_marche.py
+++ analyse_marche.py
@@ -1,15 +1,12 @@
 from scrap_book import scrap_book
 from scrap_category import scrap_category
-from scrap_list_category import scrap_list_category
-
-scrapping = {}
-nb = 0
-for book_url in scrap_category():
-    book = scrap_book(book_url)
-    if not book['category'] in scrapping:
-        scrapping[book['category']] = []
-    scrapping[book['category']].append(book)
 
 
+scrapping = {} 
 
-    
+
+for book_url in scrap_category():  # on cree la boucle pour pouvoir scrapper les livre, on va prendre un par un les url de scrap_categrory
+    book = scrap_book(book_url)  # book egale le scrap d'un book
+    if not book['category'] in scrapping:  # si il n'y a pas la  category du livre 
+        scrapping[book['category']] = []  # scrapping prend les valeur de book category
+    scrapping[book['category']].append(book)  # alors on cree la categorie du livre dans scrapping
--- scrap_category.py
+++ scrap_category.py
@@ -3,23 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrap_category():
-    for u in generate_category_url(): # Boucle qui recupère l'url et génère les url
+    for u in generate_category_url():  # Boucle qui recupère l'url et génère les url
         reponse = requests.get(u) 
         print(u)
         if reponse.ok:
             soup = BeautifulSoup(reponse.content, "html.parser")
             for item in soup.select('section h3 a'):
-                yield 'http://books.toscrape.com/catalogue/'+ item.get('href')
-            #On ajouter les url a books urls
+                yield 'http://books.toscrape.com/catalogue/' + item.get('href')
+            # On ajouter les url a books urls
         else:
             break
-
-        
-        
-
 def generate_category_url():
     nb = 0
     while True:
         nb += 1
         yield f'http://books.toscrape.com/catalogue/page-{nb}.html'
-#print(scrap_category('http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html'))
+# print(scrap_category('http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html'))
